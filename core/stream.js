5dd93b37710a49dd222ed95c6df6ad83eccfaabe
Görkem Bayraktar <bygrkm63@gmail.com>
2022-11-06 16:32:13 +0300

    atom video player indirme eklendi

--- core/stream.js
+++ core/stream.js
@@ -1,5 +1,41 @@
+const {hexToUTF8, baseURL, resolutionToHuman} = require('../core/help');
+const {requestResponseRegex, requestResponseArrayRegex, request} = require('../core/request');
 
+module.exports.stream = {
+     atom: {
+        getMovieUrl: async function(atomUrl){
+            const regex = /sources = \[\{\s*"default": true,\s*"file": "([^\s]+)"/gm;
 
-module.exports.fast = () => {
+            const data = await requestResponseRegex(
+                request(atomUrl),
+                regex
+            );
+            return hexToUTF8(data);
+        },
+        getResolutions: async function(url){
+            const base = baseURL(url);
+
+            const regex = /#EXT-X-I-FRAME-STREAM-.+RESOLUTION=([\dx]+).+URI="(.*)"/gm;
+
+            const data = await requestResponseArrayRegex(
+                request(url),
+                regex
+            );
+            
+                
+            return data.map(item => {
+                return {
+                    resolution: item[0],
+                    uri: item[1],
+                    url: [
+                        base,
+                        item[1]
+                    ].join(''),
+                    human: resolutionToHuman(item[0])
+                };
+            });
+            
     
+        }
+     }
 }
