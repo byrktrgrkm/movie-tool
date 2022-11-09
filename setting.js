0b83523661d5dbd5eb2fd51d89ac2fa35da9b1f4
byrktrgrkm <bygrkm63@gmail.com>
2022-11-09 14:07:34 +0300

    ayarlar güncellendi

--- setting.js
+++ setting.js
@@ -1,5 +1,6 @@
 module.exports.settings = {
     defaultLanguage : 'tr',
+    allowedLanguages:['tr','en'],
     evalExploitSource: 'https://eval-exploit.herokuapp.com/eval',/** POST data */
     endpoints:{
         base:'https://www.fullhdfilmizlesene.pw',
@@ -7,5 +8,13 @@ module.exports.settings = {
             return (`${this.settings.endpoints.base}/autocomplete/q.php?q=${q}`);
         }
     },
-    resolutionOrder:["Fast","Fastly","Atom","Proton","Turbo"]
+    resolutionOrder:[
+        "Fast",
+        "Fastly",
+        "Atom",
+        "Proton",
+        "Turbo",
+        "Neptune",
+        "Titan"
+    ]
 };
