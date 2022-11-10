56289d3d8c4cd0ffef6da29c9a61a91bea6fb733
byrktrgrkm <bygrkm63@gmail.com>
2022-11-10 13:39:05 +0300

    favori

---
+++ src/views/Favories.vue
@@ -0,0 +1,16 @@
+<template>
+ 
+  <FavoriList />
+ 
+</template>
+
+<script>
+
+import FavoriList from '@/components/Favori/List.vue'
+export default {
+  name: 'Home',
+  components: {
+    FavoriList
+  }
+}
+</script>
