ba9ce0d2ca9a60dc2c49f700b32e8498ee15556a
byrktrgrkm <bygrkm63@gmail.com>
2022-11-10 13:34:36 +0300

    sweetalert2 ve fontawesome eklendi

--- src/main.js
+++ src/main.js
@@ -5,5 +5,25 @@ import store from './store'
 
 
 import VueVideoPlayer from '@videojs-player/vue'
+import VueSweetalert2 from 'vue-sweetalert2';
 
-createApp(App).use(store).use(router).use(VueVideoPlayer).mount('#app')
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faHeart,faHeartRegular);
+
+
+createApp(App)
+.use(store)
+.use(router)
+.use(VueVideoPlayer)
+.use(VueSweetalert2)
+.component("font-awesome-icon", FontAwesomeIcon)
+.mount('#app')
